e2b92cf8c14eebd70048772f42fcff1c6f307e9e
jylock <jsuo.mail@gmail.com>
2015-09-27 15:55:31 -0500

    added header files for pipe.h

--- yhsrc/pipe.h
+++ yhsrc/pipe.h
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 // Execute piped commands
 void cmdPiped(Program command[],
               int numPipes,
