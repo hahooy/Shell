9ef233616027f566a40f9d7ef88a331e30eae69e
jylock <jsuo.mail@gmail.com>
2015-10-01 12:54:08 -0500

    dir_sish now utilize alphasort() to sort and list directory entries

--- yhsrc/buildin.c
+++ yhsrc/buildin.c
@@ -152,18 +152,17 @@ void help_sish(int argc, char *argv[])
 
 void dir_sish(int argc, char *argv[])
 {
-    DIR *dirp;
-	struct dirent *dp;
-
-	dirp = opendir(".");
-	if (dirp == NULL) {
-		printerr(debugLevel, "error: could not open the current directory\n");
-	return;
-    }
-	while((dp = readdir(dirp)) != NULL){
-		printf("%s\n", dp -> d_name);
-	}
-	closedir(dirp);
+ 	struct dirent **namelist;
+	int n = scandir(".", &namelist, 0, alphasort);
+    if (n < 0)
+        perror("scandir");
+    else {
+        for (int i = 0; i < n; i++) {
+            printf("%s\n", namelist[i]->d_name);
+            free(namelist[i]);
+            }
+        }
+    free(namelist);
 }
 
 void kill_sish(int argc, char *argv[])
